9688428ecdcf5cf995365ec0bb60fad293b7ad6d
Johnstone Ananda <johnolwamba@gmail.com>
2018-05-31 18:12:34 +0300

    JVM implementation added

---
+++ mppmovie-js/src/main/kotlin/com/mppmovie/Date.kt
@@ -0,0 +1,31 @@
+package com.mppmovie
+
+actual class Date actual constructor() {
+    actual fun getDate(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    actual fun getMonth(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    actual fun getFullYear(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    actual fun getHours(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    actual fun getMinutes(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    actual fun getTime(): Number {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+actual fun displayMovie(movie: Movie) {
+    console.log("JS")
+}
